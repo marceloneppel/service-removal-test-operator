63a4e52b782075dda9324f0e4089b4773ec092fa
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2024-06-24 09:46:01 -0300

    Patch K8s service
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>

--- src/charm.py
+++ src/charm.py
@@ -7,6 +7,8 @@
 import logging
 
 import ops
+from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
+from lightkube.models.core_v1 import ServicePort
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +19,8 @@ class ServiceRemovalTestOperatorCharm(ops.CharmBase):
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
         framework.observe(self.on["some_container"].pebble_ready, self._on_pebble_ready)
+        port = ServicePort(8008, name="api")
+        self.service_patcher = KubernetesServicePatch(self, [port])
 
     def _on_pebble_ready(self, event: ops.PebbleReadyEvent):
         """Handle pebble-ready event."""
